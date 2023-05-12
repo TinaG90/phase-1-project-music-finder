e8ffc34aaabd50564332800f1dd50d8bb24030c7
Altenia Gaines <gaines.tina90@gmail.com>
2023-05-12 09:26:49 -0700

    adds images to db.json

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 fetch("http://localhost:3000/music")
 .then(resp => resp.json())
-.then(json => console.log(json))
+.then(music => music.forEach(song => createSongCard(song))
+);
+
+    // const songs = document.getElementById('musicBlock')
+
+function createSongCard(){
+   let card = document.createElement('div');
+   card.classList.add('card')
+
+   let img = document.createElement('img')
+   img.src = music.image;
+   img.classList.add('album-cover');
+
+   card.append(img)
+   document.getElementById
+}
